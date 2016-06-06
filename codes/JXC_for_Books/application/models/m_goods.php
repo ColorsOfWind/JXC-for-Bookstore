131b1127172eed40422f3c624033817599fe3191
Emily <1084355983@qq.com>
2016-06-06 23:29:02 +0800

    商品

---
+++ codes/JXC_for_Books/application/models/m_goods.php
@@ -0,0 +1,68 @@
+<?php
+
+class m_goods extends CI_Model {
+
+	/*
+	**构造函数
+	*/
+	public function __construct()
+    {
+        $this->load->database();
+    }
+
+    /*
+    **添加商品
+    */
+    public function addGood($goodName, $userPassword)
+    {
+        $query = $this->db->query("INSERT INTO goods_information(inf_Barcode, inf_Classify, inf_Name, inf_Commodityprice) VALUES('$barcode', '$classify', '$name', '$price')");
+    }
+
+    /*
+    **删除商品
+    */
+    public function delGood($goodID)
+    {
+        $query = $this->db->query("DELETE FROM goods_information WHERE inf_Barcode = '$goodID'");
+    }
+
+    /*
+    **修改价钱
+    */
+    public function changePrice($goodID, $price)
+    {
+        $query = $this->db->query("UPDATE goods_information SET inf_Commodityprice = '$price' WHERE inf_Barcode = '$goodID'");
+    }
+
+    /*
+    **修改信息
+    */
+    public function changeGood($goodID, $goodClassify, $goodName, $goodAlias, $goodMnemonniccode,$goodPinyincode, $goodManufacture)
+    {
+        $query = $this->db->query("UPDATE goods_information SET inf_Classify = '$goodClassify', inf_Name = '$goodName', inf_Alias = '$goodAlias', inf_Mnemonniccode = '$goodMnemonniccode', 
+            inf_Pinyincode = '$goodPinyincode'
+            inf_Manufacture = $goodManufacture
+            WHERE inf_Barcode = '$goodID'");
+        return $query;
+    }
+
+    /*
+    **查询所有商品
+    */
+    public function checkAllGoods()
+    {
+        $query = $this->db->query("SELECT * FROM goods_information");
+        return $query;
+    }
+
+    /*
+    **查询指定商品
+    */
+    public function checkGoods($goodID)
+    {
+        $query = $this->db->query("SELECT * FROM goods_information WHERE inf_Barcode='$goodID'");
+        return $query;
+    }
+}
+
+?>
