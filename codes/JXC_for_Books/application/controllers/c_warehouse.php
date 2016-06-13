85377c8f65d7704486d71f5276640ad5d0665308
Emily <1084355983@qq.com>
2016-06-13 17:10:44 +0800

    仓库管理

---
+++ codes/JXC_for_Books/application/controllers/c_warehouse.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');	
+class c_users extends CI_Controller {
+
+  /*
+  **构造函数
+  */
+  public function __construct() 
+  {
+    parent::__construct();
+    $this -> load -> database();
+    $this -> load -> model('m_warehouses');
+  }
+
+  public function warehouses() 
+  {
+    $data = array('title' => "书店进销存管理系统");
+    $this->load->view('common/header.php',$data);
+    $this->load->view('common/menu.php');
+    $this->load->view('base/users.php');
+    $this->load->view('common/footer.php');
+  }
+
+  /*
+  **添加仓库
+  */
+  public function addWarehouse()
+  {
+    $warehouseName = $this -> input -> post('warehouseName');
+    $warehousePinyin = $this -> input->post('warehousePinyin');
+    $warehouseLocation = $this -> input->post('warehouseLocation');
+    $warehouseDescribe = $this -> input->post('warehouseDescribe');
+
+    $query = $this -> m_warehouses -> checkWarehousesName($warehouseName);
+    if($query -> num_rows() == 0)
+    {
+      $this -> m_warehouses -> addWarehouse($warehouseName, $warehousePinyincode, $warehouseLocation, $warehouseDescribe);
+      $data = array('msg' => 'true');
+    }
+    else $data = array('msg' => 'false');
+    $this->output -> set_content_type('application/json') -> set_output(json_encode($data));
+  }
+
+  /*
+  **删除仓库
+  */
+  public function delWarehouse()
+  {
+    $warehouseID = $this -> input -> post('warehouseID');
+
+    $query = $this -> m_warehouses -> checkWarehousesID($warehouseID);
+    if($query -> num_rows() != 0)
+    {
+      $this -> m_warehouses -> delWarehouse($warehouseID);
+      $data = array('msg' => 'true');
+    }
+    else
+    {
+      $data = array('msg' => 'false');
+    }
+    $this->output
+    ->set_content_type('application/json')
+    ->set_output(json_encode($data));
+  }
+
+  /*
+  **查找所有用户
+  */
+  public function checkAllWarehouses()
+  {
+    $allInfo = $this -> m_warehouses -> checkAllWarehouses();
+    $result = $allInfo -> result();
+    $data = array('data' => $result);
+    $this->output
+    ->set_content_type('application/json')
+    ->set_output(json_encode($data));
+  }
+
+  /*修改仓库信息*/
+  public function changeWarehouse()
+  {
+    $warehouseID = $this -> input -> post('warehouseID');
+    $warehouseName = $this -> input -> post('warehouseName');
+    $warehousePinyin = $this -> input->post('warehousePinyin');
+    $warehouseLocation = $this -> input->post('warehouseLocation');
+    $warehouseDescribe = $this -> input->post('warehouseDescribe');
+
+    $query = $this -> m_warehouses -> changeWarehouse($warehouseID, $warehouseName, $warehousePinyin, $warehouseLocation, $warehouse_Describe);
+  }
+}
+?>
